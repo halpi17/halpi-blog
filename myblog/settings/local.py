1b967b4ae7a52efbf5f64feea9e04954be9c8216
nishiyamaymyt@gmail.com <pi_ano111@Tomoki-Nishiyama.local>
2021-07-16 21:45:17 +0900

    Del: local_settings.py

--- myblog/settings/local.py
+++
@@ -1,9 +0,0 @@
-from .base import *
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!0zl3l(4gx2(rm$36ww!_z(fd_%#(+&09bsum_2(@-)bpkg%x6'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
