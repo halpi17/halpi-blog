ee0a2ce44454470324485d7ff9d53ed1a77a44f5
halpi17 <pi_ano111@Tomoki-Nishiyama.local>
2021-07-31 14:54:01 +0900

    add: mdeditor settings

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
 from django.conf import settings
+from django.conf.urls import url, include
 from django.conf.urls.static import static
 
 from blog.views import (
@@ -34,7 +35,8 @@ urlpatterns = [
     path('reply/<int:pk>/', ReplyFormView.as_view(), name='reply_form'),
     path('reply/<int:pk>/approve/', reply_approve, name='reply_approve'),
     path('reply/<int:pk>/remove/', reply_remove, name='reply_remove'),
+    url(r'mdeditor/', include('mdeditor.urls')) # 追加
 ]
 
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
