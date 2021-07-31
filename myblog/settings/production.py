3294cc889bb79040b6742ad81a38cc9d80ed15e8
halpi17 <pi_ano111@Tomoki-Nishiyama.local>
2021-07-31 12:06:27 +0900

    .

--- myblog/settings/production.py
+++ myblog/settings/production.py
@@ -9,6 +9,6 @@ DEBUG = False
 ALLOWED_HOSTS = ['*']
 # ALLOWED_HOSTS = ['halpi17.com', '.halpi17.com']
 
-# import dj_database_url
-# db_from_env = dj_database_url.config(conn_max_age=600)
-# DATABASES['default'].update(db_from_env)
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
