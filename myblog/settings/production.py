f21e2b83985ae00ff0bb776d5e717821a923b2d3
halpi17 <pi_ano111@Tomoki-Nishiyama.local>
2021-07-31 12:05:02 +0900

    .

--- myblog/settings/production.py
+++ myblog/settings/production.py
@@ -6,7 +6,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 # ALLOWED_HOSTS = ['halpi17.com', '.halpi17.com']
 
 # import dj_database_url
